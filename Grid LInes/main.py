80c0f080cac0a787f21058e8f8b4dab906d86380
404Shihab <shihabbhuiyan500@gmail.com>
2026-09-23 23:30:16 +0600

    feat: add grid lines to enhance plot readability

---
+++ Grid LInes/main.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# grid() = Helps make plots easier to read by adding reference lines
+
+x = [1,2,3,4,5]
+y=[5,10,15,20,25]
+
+# plt.grid(axis="x") # for single axis
+# plt.grid(axis="y") # for single axis
+
+plt.grid(axis="y",
+         linewidth="2",
+         color="lightgray",
+         linestyle="dotted")
+
+# plt.grid()
+
+
+plt.plot(x,y)
+
+plt.show()
+
