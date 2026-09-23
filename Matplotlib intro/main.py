f99473141dbd285a5236fa82d92afcece411f414
404Shihab <shihabbhuiyan500@gmail.com>
2026-09-23 12:22:14 +0600

    add initial plotting example using matplotlib

---
+++ Matplotlib intro/main.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+# pyplot provides a user-friendly interface for plotting
+
+# print(matplotlib.__version__)
+
+x= [2022,2023,2024,2025]
+y = [12, 23, 19, 22]
+
+plt.plot(x,y)
+
+plt.show()
