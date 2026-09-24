5503e00b17f1cf590c43becaed2f78f9c3ce1d6a
404Shihab <shihabbhuiyan500@gmail.com>
2026-09-24 09:32:10 +0600

    feat: add scatter plot example for study hours vs test scores

---
+++ Scatter plots/main.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# scatter graph = Shows the relationship between two variables
+# Helps to identify a correlation (+,-, None)
+# Example: Study hours vs. Test scores
+
+x1 = np.array([0, 1, 1, 2, 3, 4, 5, 6, 7, 7, 8]) # hours study
+y1 = np.array([55,60,65,62,68,70,75,78,82,85,87]) # grades
+
+x2= np.array ([0, 1, 2, 2, 3, 4, 5, 6, 7, 8, 8]) # Hours studied
+y2 = np.array([50, 58, 65, 70, 72, 78, 83, 88, 92, 95, 97]) #Grades
+
+plt.scatter(x1,y1, color="red",
+                 alpha=0.5, # transparency
+                 s = 200,
+                 label="Class A"
+                 )
+
+plt.scatter(x2,y2, color="skyblue",
+                 alpha=0.5, # transparency
+                 s = 200,
+                 label="Class B"
+                 )
+
+plt.legend()
+plt.title("Test Scores")
+plt.xlabel("Hours Studied")
+plt.ylabel("Grades")
+
+
+
+plt.show()
+
